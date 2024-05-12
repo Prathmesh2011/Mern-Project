5974a025eda045871daf2262530fae9cf43f94b6
PrathmeshRewatkar20 <prathmeshrewatkar2011@gmail.com>
2024-05-12 09:53:49 +0530

    Create get posts api route and show user posts inside dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -28,7 +27,6 @@ function App() {
       <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
       </Route>
-      <Route path='/projects' element={<Projects />} />
     </Routes>
     <FooterOn/>
   </BrowserRouter>
